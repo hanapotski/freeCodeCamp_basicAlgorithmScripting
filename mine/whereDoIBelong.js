9fd42543b96e0a90d8ba1cc99b5d48b13c8b0a38
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-26 21:40:54 -0700

    where do I belong

--- mine/whereDoIBelong.js
+++ mine/whereDoIBelong.js
@@ -9,3 +9,58 @@ function getIndexToIns(arr, num) {
 }
 
 getIndexToIns([2, 5, 10], 15);
+
+//Other Solutions
+
+function getIndexToIns(arr, num) {
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+
+  for (var a = 0; a < arr.length; a++) {
+    if (arr[a] >= num)
+      return a;
+  }
+
+  return arr.length;
+}
+
+
+function getIndexToIns(arr, num) {
+  // Find my place in this sorted array.
+  var times = arr.length; // runs the for loop once for each thing in the array
+  var count = 0;
+  for (i=0;i<times;i++){
+    if(num>arr[i]){count++;} } // counts how many array numbers are smaller than num
+    return count; // the above equals num's position in a sorted array
+}
+
+getIndexToIns([40, 60], 50);
+
+
+function getIndexToIns(arr, num) {
+
+  arr.sort(function(a, b) {
+  return a - b;
+  });
+
+  var i = 0;
+  while (num > arr[i]) {
+  i++;
+  }
+
+  return i;
+}
+
+
+getIndexToIns([40, 60], 50);
+
+function getIndexToIns(arr, num) {
+  // sort and find right index
+  var index = arr.sort((curr, next) => curr > next)
+    .findIndex((currNum)=> num <= currNum);
+  // Returns proper answer
+  return index === -1 ? arr.length : index;
+}
+
+getIndexToIns([40, 60], 500);
