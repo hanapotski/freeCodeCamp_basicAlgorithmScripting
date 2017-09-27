e7c34a739024fcdd4aa30ae0f90f436cac5b6481
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-26 21:18:28 -0700

    seek and destroy

---
+++ mine/seekAndDestroy.js
@@ -0,0 +1,21 @@
+function destroyer(arr) {
+  var args = Array.prototype.slice.call(arguments);
+
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < args.length; j++) {
+      if (arr[i] === args[j]) {
+        delete arr[i];
+      }
+    }
+  }
+  return arr.filter(Boolean);
+}
+
+
+//Other Solution
+function destroyer(arr) {
+  var args = Array.from(arguments).slice(1);
+  return arr.filter(function(val) {
+    return !args.includes(val);
+  });
+}
