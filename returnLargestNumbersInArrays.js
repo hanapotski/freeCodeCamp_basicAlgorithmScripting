96954da6c08e59bcef8b9d58977e0055270904eb
hanapotski <hannahjoy.zulueta@gmail.com>
2018-02-18 23:50:20 -0800

    Return Largest Numbers in Arrays

---
+++ returnLargestNumbersInArrays.js
@@ -0,0 +1,7 @@
+function largestOfFour(mainArray) {
+  return mainArray.map(function(subArray) {
+    return Math.max.apply(null, subArray);
+  });
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
